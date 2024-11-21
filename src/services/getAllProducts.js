ff5227fccc7dd19f6facb4a5f0d2c912e2df39cd
Muhammad Rafvy <rafvy.muhammad@gmail.com>
2024-11-21 08:37:56 +0700

    Menambahkan 2 Produk baru

--- src/services/getAllProducts.js
+++ src/services/getAllProducts.js
@@ -30,6 +30,25 @@ export const getAllProducts = () => {
       imageUrl: `/assets/images/WMNS+AIR+FORCE+1+'07.jpeg`,
       description: `Comfortable, durable and timeless—it's number 1 for a reason. The classic '80s construction pairs a warm suede upper with neutral accents for style that tracks whether you're on court or on the go.`
     },
-
-  ]
+    {
+      id: 'NAF-1-07-FMEN-004',
+      slug: 'nike-air-force-1-07-red',
+      name: "Nike Air Force 1 '07 Red",
+      category: 'Men\'s Shoe',
+      price: 1_699_000,
+      stock: 20,
+      imageUrl: '/assets/images/AIR-FORCE-1-07-RED.png',
+      description: `Stand out in the crowd with the Nike Air Force 1 '07 Red, featuring a bold red colorway and classic silhouette.`
+    },
+    {
+      id: 'NAF-1-07-FWOMEN-005',
+      slug: 'nike-air-force-1-07-pink',
+      name: "Nike Air Force 1 '07 Pink",
+      category: 'Women\'s Shoe',
+      price: 1_749_000,
+      stock: 15,
+      imageUrl: '/assets/images/AIR-FORCE-1-07-PINK.png',
+      description: `Elevate your style with the Nike Air Force 1 '07 Pink, perfect for a chic and trendy look.`
+    },
+  ];
 }
